9c8e7483c5b637ad8b225a962d3dc497356e0197
anna-java <annbadnyc@gmail.com>
2024-03-05 21:25:51 -0600

    To add scanner to OddEven class

--- src/main/java/algorithms/OddEven.java
+++ src/main/java/algorithms/OddEven.java
@@ -1,4 +1,4 @@
-/* В классе OddEven создать алгоритм oddEven(), который принимает на вход целое число, возвращает “Odd”,
+/* В классе NeedToRename создать алгоритм oddEven(), который принимает на вход целое число, возвращает “Odd”,
 если число нечетное, и “Even”, если число четное.
 
 Test Data:
@@ -8,14 +8,24 @@ Test Data:
 
 package algorithms;
 
+import java.util.Scanner;
+
 public class OddEven {
 
     public String oddEven(int num) {
 
-        if (num % 2 != 0) {
-            return "Odd";
-        } else {
+        System.out.println("Enter a number: ");
+
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        if (num % 2 == 0) {
             return "Even";
+        } else {
+            return "Odd";
         }
     }
+
 }
+
+
---
+++ src/main/java/algorithms/OddIndices.java
@@ -0,0 +1,12 @@
+/* Написать алгоритм OddIndices, который принимает массив целых чисел,  и возвращает массив значений нечетных индексов
+Test Data:
+Input = {-45, 590, 234, 985, 12, 68}
+Expected Result =  {590, 985, 68}
+*/
+
+package algorithms;
+
+public class OddIndices {
+
+
+}
