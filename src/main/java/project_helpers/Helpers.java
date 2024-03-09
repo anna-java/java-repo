8d216b279e9f550ac825823c01ed8cf381bbf127
anna-java <annbadnyc@gmail.com>
2024-03-09 15:50:53 -0600

    To add methods to class Helpers

--- src/main/java/project_helpers/Helpers.java
+++ src/main/java/project_helpers/Helpers.java
@@ -2,8 +2,39 @@ package project_helpers;
 
 public class Helpers {
 
-    public static void main(String[] args) {
+    // Methods to check whether an array  is empty (int, double, String)
 
+    public static boolean checkIfArrayIsEmpty(int[] array) {
+
+        if (array.length != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean checkIfArrayIsEmpty(double[] array) {
+
+        if (array.length != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean checkIfArrayIsEmpty(String[] array) {
+
+        if (array.length != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
 
     }
 }
