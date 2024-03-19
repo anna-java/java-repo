03a16db67ebb9653546466611d0f3c09458f2536
anna-java <annbadnyc@gmail.com>
2024-03-19 12:53:55 -0500

    To write AreNumbersEqual algo, to add Instructions.html

---
+++ src/main/java/algorithms/AreNumbersEqual.java
@@ -0,0 +1,30 @@
+/*
+Напишите алгоритм AreNumbersEqual, который принимает на вход 2 любых int числа, и возвращает
+0, если числа равны
+-1, если первое число меньше второго
+1, если первое число больше второго
+Test Data:
+89, 89
+Expected result: 0
+-89, 89
+Expected result: -1
+89, -89
+Expected result: 1
+ */
+
+
+package algorithms;
+
+public class AreNumbersEqual {
+
+    public int areNumbersEqual(int num1, int num2) {
+
+        if (num1 == num2) {
+            return 0;
+        } else if (num1 < num2) {
+            return - 1;
+        } else {
+            return 1;
+        }
+    }
+}
--- src/test/java/IsPositiveNumberTest.java
+++ src/test/java/IsPositiveNumberTest.java
@@ -7,7 +7,7 @@ public class IsPositiveNumberTest {
     @Test
     public void positiveNum_HappyPath() {
 
-        int num = 11;
+        int num = 555;
         boolean expectedResult = true;
 
         boolean actualResult = new IsPositiveNumber().isPositiveNumber(num);
@@ -19,7 +19,7 @@ public class IsPositiveNumberTest {
     @Test
     public void negativeNum_HappyPath() {
 
-        int num = -93;
+        int num = -555;
         boolean expectedResult = false;
 
         boolean actualResult = new IsPositiveNumber().isPositiveNumber(num);
