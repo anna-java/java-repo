6baaa420479e7e014f3ae25b88b45539beecb203
anna-java <annbadnyc@gmail.com>
2024-03-21 14:58:13 -0500

    To fix ConditionalActions algo

--- src/main/java/algorithms/ConditionalActions.java
+++ src/main/java/algorithms/ConditionalActions.java
@@ -13,14 +13,13 @@ public class ConditionalActions {
 
     public String conditionalActions(int M) {
 
-        if ((M % 7 = 0) && (M % 9 = 0)) {
+        if ((M % 7 == 0) && (M % 9 == 0)) {
             return "Good Number";
-        } else if ((M % 7 = !0) && (M % 9 = 0)) {
+        } else if ((M % 7 != 0) && (M % 9 == 0)) {
             return "Bad Number";
-        } else if (M % 11 = 0) {
+        } else if (M % 11 == 0) {
             return "Poor Number";
-        } else {
-            return "-1";
         }
+            return "-1";
     }
 }
