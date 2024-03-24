c2758042b31bad1d6bdd71f49b164fcb1c6aad2f
anna-java <annbadnyc@gmail.com>
2024-03-24 17:27:26 -0500

    To write createDoubleArray method and test

--- src/main/java/algorithms/CreateArray.java
+++ src/main/java/algorithms/CreateArray.java
@@ -4,6 +4,16 @@
 1. В голубой папке java создать класс CreateArray, написать в этом классе метод createIntArray(), который принимает
 на вход 5 целых чисел, и возвращает массив из этих же чисел.
 Например, createIntArray(1, 2, 3, 4, 5) -> {1, 2, 3, 4, 5}
+
+2. Написать метод createDoubleArray(), который принимает на вход 5 чисел типа double, и возвращает массив из этих же чисел
+Например, createDoubleArray(1.1, 2.5, 3.7, 4.0, 5.5) -> {1.1, 2.5, 3.7, 4.0, 5.5}
+
+3. Написать метод createStringArray(), который принимает на вход 5 слов, и возвращает массив из этих слов
+Например, createStringArray(“It”, “was”, “an”, “apple”, “pie”) -> {“It”, “was”, “an”, “apple”, “pie”}
+
+4. 4-aя задача только для тех, кто хочет разобраться с методом класса String split()
+Написать метод createArrayFromText(), который принимает на вход предложение из нескольких слов и возвращает массив из этих слов.
+Например, createArrayFromText(“It was an apple pie”) -> {“It”, “was”, “an”, “apple”, “pie”}
  */
 
 
@@ -14,5 +24,11 @@ public class CreateArray {
     public int[] createIntArray(int num1, int num2, int num3, int num4, int num5) {
 
         return new int[]{num1, num2, num3, num4, num5};
+    }
+
+
+    public double[] createDoubleArray(double num1, double num2, double num3, double num4, double num5) {
+
+        return new double[]{num1, num2, num3, num4, num5};
     }
 }
--- src/test/java/CreateArrayTest.java
+++ src/test/java/CreateArrayTest.java
@@ -14,4 +14,15 @@ public class CreateArrayTest {
 
         Assert.assertEquals(actualResult, expectedResult);
     }
+
+    @Test
+    public void createDoubleArray() {
+
+        double num1 = 1.1, num2 = 2.5, num3 = 3.7, num4 = 4.0, num5 = 5.5;
+        double[] expectedResult = {1.1, 2.5, 3.7, 4.0, 5.5};
+
+        double[] actualResult = new CreateArray().createDoubleArray(num1, num2, num3, num4, num5);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
