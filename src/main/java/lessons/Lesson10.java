e15cdc332046bca1700c00be4ad671fad35d1397
anna-java <annbadnyc@gmail.com>
2024-04-20 12:47:54 -0500

    Comment all the code in Lesson10

--- src/main/java/lessons/Lesson10.java
+++ src/main/java/lessons/Lesson10.java
@@ -191,86 +191,82 @@ public class Lesson10 {
 //
 
 
-
-
 //_________________________String Concatenation vs .concat() vs StringBuilder vs StringBuffer___________________________
-        String line = "_________________________________________________________";
-        String ll = "|\t";
-        String lr = "\t|";
+//        String line = "_________________________________________________________";
+//        String ll = "|\t";
+//        String lr = "\t|";
 //
-        String str1 = "QA";
-        int num1 = 4;
-        String str2 = "Every";
-        int num2 = 1;
-
-        long start = 0;
-        long end = 0;
+//        String str1 = "QA";
+//        int num1 = 4;
+//        String str2 = "Every";
+//        int num2 = 1;
 //
-        String resultPlus = "";
-        start = System.nanoTime();
-        resultPlus = str1 + num1 + str2 + num2;
-        end = System.nanoTime();
-
-        System.out.println(line);
-        System.out.println(ll + "result\t\t" + lr + ll + "Concatenation" + lr + ll + "Time" + "\t" + lr);
-        System.out.println(line);
-        System.out.println(ll + "resultPlus\t" + lr + ll + resultPlus + "\t" + lr + ll + (end - start) + lr);
-        System.out.println(line);
-
-        String resultConcat = "";
-        start = 0;
-        start = System.nanoTime();
-
-        resultConcat = resultConcat
-                .concat(str1)
-                .concat(String.valueOf(num1))
-                .concat(str2)
-                .concat(String.valueOf(num2));
-
-        end = System.nanoTime();
+//        long start = 0;
+//        long end = 0;
+////
+//        String resultPlus = "";
+//        start = System.nanoTime();
+//        resultPlus = str1 + num1 + str2 + num2;
+//        end = System.nanoTime();
 //
-        System.out.println(ll + "resultConcat" + lr + ll + resultConcat + "\t" + lr + ll + (end - start) + "\t" + lr);
-        System.out.println(line);
-
-
-
-        StringBuilder resultSB = new StringBuilder();
-
-        start = System.nanoTime();
-
-        resultSB
-                .append(str1)
-                .append(num1)
-                .append(str2)
-                .append(num2);
-
-        end = System.nanoTime();
-
-        System.out.println(ll + "resultSB" + "\t" + lr + ll + resultSB + "\t" + lr + ll + (end - start) + "\t" + lr);
-        System.out.println(line);
-
-
-
-        StringBuffer resultSBuf = new StringBuffer();
-        start = System.nanoTime();
-
-        resultSBuf
-                .append(str1)
-                .append(num1)
-                .append(str2)
-                .append(num2);
-
-        end = System.nanoTime();
-
-        System.out.println(ll + "resultSBuf" + "\t" + lr + ll + resultSBuf + "\t" + lr + ll + (end - start) + "\t" + lr);
-        System.out.println(line);
-
-        System.out.println(resultPlus.equals(resultConcat));
-        System.out.println(resultPlus.equals(resultSB.toString()));
-
-
-    }
-}
+//        System.out.println(line);
+//        System.out.println(ll + "result\t\t" + lr + ll + "Concatenation" + lr + ll + "Time" + "\t" + lr);
+//        System.out.println(line);
+//        System.out.println(ll + "resultPlus\t" + lr + ll + resultPlus + "\t" + lr + ll + (end - start) + lr);
+//        System.out.println(line);
+//
+//        String resultConcat = "";
+//        start = 0;
+//        start = System.nanoTime();
+//
+//        resultConcat = resultConcat
+//                .concat(str1)
+//                .concat(String.valueOf(num1))
+//                .concat(str2)
+//                .concat(String.valueOf(num2));
+//
+//        end = System.nanoTime();
+////
+//        System.out.println(ll + "resultConcat" + lr + ll + resultConcat + "\t" + lr + ll + (end - start) + "\t" + lr);
+//        System.out.println(line);
+//
+//
+//
+//        StringBuilder resultSB = new StringBuilder();
+//
+//        start = System.nanoTime();
+//
+//        resultSB
+//                .append(str1)
+//                .append(num1)
+//                .append(str2)
+//                .append(num2);
+//
+//        end = System.nanoTime();
+//
+//        System.out.println(ll + "resultSB" + "\t" + lr + ll + resultSB + "\t" + lr + ll + (end - start) + "\t" + lr);
+//        System.out.println(line);
+//
+//
+//
+//        StringBuffer resultSBuf = new StringBuffer();
+//        start = System.nanoTime();
+//
+//        resultSBuf
+//                .append(str1)
+//                .append(num1)
+//                .append(str2)
+//                .append(num2);
+//
+//        end = System.nanoTime();
+//
+//        System.out.println(ll + "resultSBuf" + "\t" + lr + ll + resultSBuf + "\t" + lr + ll + (end - start) + "\t" + lr);
+//        System.out.println(line);
+//
+//        System.out.println(resultPlus.equals(resultConcat));
+//        System.out.println(resultPlus.equals(resultSB.toString()));
+//    }
+//}
 
 //        long t1 = System.nanoTime();
 //        str1.concat(str2);
@@ -286,5 +282,5 @@ public class Lesson10 {
 //        sb1.append(sb2);
 //        long t4 = System.nanoTime();
 //        System.out.println(t4 - t3);
-
-
+    }
+}
