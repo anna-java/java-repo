55c60ac37f55830001cb0e5366cd66cad083529a
anna-java <annbadnyc@gmail.com>
2024-05-08 19:40:41 -0500

    Add BubbleSort algorithm with boolean and while

---
+++ src/main/java/algorithms/BubbleSort.java
@@ -0,0 +1,35 @@
+// BubbleSort with boolean and while
+
+package algorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i++) {
+
+                if (array[i - 1] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] main) {
+
+        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
