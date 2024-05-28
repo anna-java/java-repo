19b915d9a40e373bf972d0504cb437d76c1905ee
anna-java <annbadnyc@gmail.com>
2024-05-28 11:16:00 -0500

    Add code to Lesson5Part2

--- src/test/java/ToSentenceCaseTest.java
+++ src/test/java/ToSentenceCaseTest.java
@@ -5,14 +5,15 @@ import org.testng.annotations.Test;
 
 public class ToSentenceCaseTest {
 
-    private static final String SENTENCE = "Mother";
+    //private static final String SENTENCE = "Mom";
     private static final String URL = "https://capitalizemytitle.com";
+    private static final String TEST_SENTENCE = "mom is cleaning the Window";
 
 
     @Test
     public void testCapitalizeMyTitle() {
 
-        final String expectedResult = new ToSentenceCase().toSentenceCase(SENTENCE);
+        final String expectedResult = new ToSentenceCase().toSentenceCase(TEST_SENTENCE);
 
         WebDriver driver = new ChromeDriver();
         driver.get(URL);
