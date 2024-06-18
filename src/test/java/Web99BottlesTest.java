cece4fd58c65de871fc91d52941701d785414d44
anna-java <annbadnyc@gmail.com>
2024-06-17 19:35:05 -0500

    Update Web99BottlesTest

--- src/test/java/Web99BottlesTest.java
+++ src/test/java/Web99BottlesTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,7 +9,11 @@ public class Web99BottlesTest {
     public void test99Bottles() {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.99-bottles-of-beer.net");
-    }
 
+        driver.get("https://www.99-bottles-of-beer.net");
+        driver.findElement(By.xpath("//ul[@id='menu']//a[@href='/']")).click();
+        driver.findElement(By.linkText("Song Lyrics")).click();
+
+        driver.quit();
+    }
 }
