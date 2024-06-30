b1616b86e71673dd3dcfeb5c010718060c173b2d
anna-java <annbadnyc@gmail.com>
2024-06-30 17:43:01 -0500

    Update LumaTopJacketsTest

--- src/test/java/LumaTopJacketsTest.java
+++ src/test/java/LumaTopJacketsTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
@@ -7,14 +8,19 @@ import org.testng.annotations.Test;
 public class LumaTopJacketsTest {
 
     @Test
-    public void testLumaTopJacketsTest() {
+    public void testLumaTopJacketsTest() throws InterruptedException {
 
-        WebDriver driver = new EdgeDriver();
+        //WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
+
         driver.get("https://magento.softwaretestingboard.com/");
-        driver.findElement(By.xpath("//span[text()='Women']/preceding-sibling::span")).click();
+        Thread.sleep(2000);
 
-        //span[@class='ui-menu-icon ui-icon ui-icon-carat-1-e']
-        //span[text()='Women']/preceding-sibling::span
+        driver.findElement(By.xpath("//span[text()='Women']/preceding-sibling::span")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//span[text()='Women']/ancestor::li//span[text()='Tops']/preceding-sibling::span"));
+
 
     }
 }
