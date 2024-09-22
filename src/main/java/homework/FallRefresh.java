c33e87d42f3ac3c6e7bbbdc58d2aba4550f37bb9
anna-java <annbadnyc@gmail.com>
2024-09-22 10:48:37 -0500

    Update FallRefresh.java class

--- src/main/java/homework/FallRefresh.java
+++ src/main/java/homework/FallRefresh.java
@@ -10,7 +10,7 @@ public class FallRefresh {
 
 
         System.out.println("Task #1_2:");
-        System.out.println("____888888______________________________\n" +
+        System.out.print("____888888______________________________\n" +
                 "____888888______________________________\n" +
                 "___8888888888___________________________\n" +
                 "__888888888888__________________________\n" +
@@ -45,7 +45,7 @@ public class FallRefresh {
 
 
         System.out.println("Task #1_3");
-        System.out.println("( > ^ . ^ < )\n" +
+        System.out.print("( > ^ . ^ < )\n" +
                 "\t\t\t\tC( - ___ - C )\n" +
                 "( 0 _._ 0 )");
         System.out.println();
