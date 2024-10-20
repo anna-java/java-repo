7f3a97ca1ccb86e9a1365b45769e002d86a47295
anna-java <annbadnyc@gmail.com>
2024-10-20 13:22:11 -0500

    Add task #5_3 to FallRefresh class

--- src/main/java/homework/FallRefresh.java
+++ src/main/java/homework/FallRefresh.java
@@ -375,6 +375,22 @@ public class FallRefresh {
         System.out.println(max);
 
 
+        System.out.println("\nTask #5_3:");
+        /*
+        Дан массив:
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        необходимо вывести минимальное значение массива.
+         */
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
+
+
 
 
 
