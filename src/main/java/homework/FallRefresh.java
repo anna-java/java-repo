694079883e9b8b2738d7d9d2e874400f3b411e0d
anna-java <annbadnyc@gmail.com>
2024-10-26 22:47:05 -0500

    Fixed solution for task #5_12:

--- src/main/java/homework/FallRefresh.java
+++ src/main/java/homework/FallRefresh.java
@@ -567,21 +567,12 @@ public class FallRefresh {
          */
         int rows = 10;
 
-        for (int i = 0; i < rows + 1; i++) {
+        for (int i = rows; i > -1; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
-
-//            int rows = 5;
-//
-//            for(int i = 1; i <= rows; ++i) {
-//                for(int j = 1; j <= i; ++j) {
-//                    System.out.print(j + " ");
-//                }
-//                System.out.println();
-//            }
 
 
     }
