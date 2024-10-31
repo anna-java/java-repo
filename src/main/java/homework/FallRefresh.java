15dee6794aa93dce6d10875cf98909e66bd71d76
anna-java <annbadnyc@gmail.com>
2024-10-31 16:31:43 -0500

    Add partial solution for #5_14

--- src/main/java/homework/FallRefresh.java
+++ src/main/java/homework/FallRefresh.java
@@ -661,7 +661,7 @@ public class FallRefresh {
         }
 
 
-        System.out.println("\nTask #5_13:");
+        System.out.println("\nTask #5_14:");
         /*
         9 8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8 9
           8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8
@@ -674,6 +674,15 @@ public class FallRefresh {
                         1 0 1
                           0
          */
+        for (int i = 9; i > -1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 9; j > i - 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
 
 
