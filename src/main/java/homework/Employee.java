6cc4d409b300faa78d99347e8d40c62e779ef5c2
anna-java <annbadnyc@gmail.com>
2024-11-17 13:44:09 -0600

    Add tasks to HW6 Create Employee class

---
+++ src/main/java/homework/Employee.java
@@ -0,0 +1,15 @@
+/*
+Задача №1
+
+Необходимо создать класс Employee со следующими методами:
+getBaseSalary - получить базовую ставку
+setBaseSalary
+getName - получить имя
+setName
+getSalary - получить зарплату
+ */
+
+package homework;
+
+public class Employee {
+}
