43e8c42a46a96df00f25c4493cc8c820b3e8aceb
anna-java <annbadnyc@gmail.com>
2024-12-04 16:01:00 -0600

    Add solution for the task#8_2

---
+++ src/main/java/homework/TaxPayment.java
@@ -0,0 +1,29 @@
+package homework;
+
+public class TaxPayment {
+
+    String name;
+    static double incomeTax;
+    static int numOfKids;
+
+    public static double calculateIncomeTax(Employee2[] employees) {
+        if (employees.length == 0) {
+            System.out.println("No employees were found");
+            return -1;
+        }
+        double salary;
+        if (salary <= 0) {
+            System.out.println("Salary cannot be a negative number");
+            return -1;
+        } else if (salary > 0 && salary < 5000) {
+            incomeTax = (salary - numOfKids * 1000) * 0.13;
+            return incomeTax;
+        } else if (salary >= 5000 && salary <= 10000) {
+            incomeTax = (salary - numOfKids * 1000) * 0.18;
+            return incomeTax;
+        } else {
+            incomeTax = (salary - numOfKids * 1000) * 0.23;
+            return incomeTax;
+        }
+    }
+}
