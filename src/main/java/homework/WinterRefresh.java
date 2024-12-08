92036bf4dcafe92e088d4a382b85a28f8e544cd4
anna-java <annbadnyc@gmail.com>
2024-12-08 17:41:48 -0600

    Add tasks for #87 to WinterRefresh

--- src/main/java/homework/WinterRefresh.java
+++ src/main/java/homework/WinterRefresh.java
@@ -4,6 +4,25 @@ public class WinterRefresh {
 
     /* Задача №1
 
+Необходимо написать 4 метода:
+сложение 2х чисел
+вычитание 2х чисел
+умножение 2х чисел
+деление 2х чисел
+*/
+
+
+/* Задачи №2
+
+https://www.codewars.com/kata/53ee5429ba190077850011d4/train/java
+https://www.codewars.com/kata/555086d53eac039a2a000083/train/java
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/java
+https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/java
+https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/java
+     */
+
+    /* Задача №1
+
 Написать статический метод, превращающий натуральное (целое положительное) число (например, 643) в строку,
 состоящую из названий цифр этого числа (например, “шесть четыре три”).
 */
